2f3dc868843eb1d5e4b833b3217f9e706d5bd73f
Hemant Kumar <63533985+Hemant1101@users.noreply.github.com>
2022-10-06 14:22:06 +0530

    Boston House Price Prediction ML model

---
+++ Machine Learning Projects/Boston Housing Prediction/README.md
@@ -0,0 +1,16 @@
+## Boston House Pricing Prediction
+
+ml learning linear regression model deployment boston house pricing
+
+### Software And Tools Requirements
+
+1. GitHub Account
+2. Heroku Account
+3. VS Code IDE
+4. Git Cli
+
+Create a new environment
+
+```
+conda create -p venv python==3.7 -y
+```
---
+++ Machine Learning Projects/Boston Housing Prediction/app.py
@@ -0,0 +1,36 @@
+from distutils.log import debug
+import pickle
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+import math
+
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    output=math.floor(regmodel.predict(final_input)[0])
+    return render_template("home.html",prediction_text="The House price prediction is {} Lakhs".format(output))
+
+if __name__=="__main__":
+    app.run(debug=True)
